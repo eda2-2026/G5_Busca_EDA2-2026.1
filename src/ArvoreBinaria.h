993cef895c7d8897b711a7f0b4a34c3638be2c26
Luiz <luizguilermemoraiscosta@gmail.com>
2026-04-03 15:59:37 -0300

    Adiciona versões inciais das classes: ArvoreBinaria, NoArvore e Veiculo

---
+++ src/ArvoreBinaria.h
@@ -0,0 +1,24 @@
+#include <string>
+#include "NoArvore.h"
+#include "Veiculo.h"
+
+class ArvoreBinaria {
+
+private:
+    NoArvore* raiz = nullptr;
+
+
+public:
+
+    void InserirVeiculo(Veiculo *veiculo) {
+
+    }
+
+    void DeletarVeiculo(std::string placa) {
+
+    }
+
+    Veiculo BuscarVeiculo(std::string placa) {
+        return nullptr;
+    }
+};
